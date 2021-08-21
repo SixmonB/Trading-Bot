58541d69f7b7aa95ca329002a6dbfec7f4f506a5
JuanPabloBrondo <jpbrondo1@gmial.com>
2021-08-21 20:36:20 -0300

    Trabajamos en la segunda condición

--- main.py
+++ main.py
@@ -21,14 +21,16 @@ import config, websocket, json, numpy, talib, pprint
 
 SOCKET = "wss://stream.binance.com:9443/ws/adausdt@kline_15m"    # <symbol>@kline<interval>
 closes = []
-
+closesHace15 = []
 # ------------- PARAMETROS -------------
  
-EMA_period = 17*(60/2)
-APO_slow = 20*(60/2) # 10 y 20 para velas de 15 minutos
-APO_fast = 10*(60/2)
+EMA_period = 17*(15*60/2)
+APO_slow = 20*(15*60/2) # 10 y 20 para velas de 15 minutos
+APO_fast = 10*(15*60/2)
 matype = 0
-
+APO_delta=-0.0025 #Posibilidad de que varíe en función del precio (podría ser por rango de precio)
+            #Precio   $(0.66-1) ; $(1-1.33) ; $(1.33-1.66) ; $(1.66-2)
+         #APO_delta    -0.0012     -0.0014      -0.0025 
 # --------------------------------------
 
 def on_open(ws):
@@ -53,10 +55,16 @@ def on_message(ws, message):
     if len(closes) > APO_slow:
         my_apo = talib.APO(closes, APO_fast, APO_slow, matype)
         if(my_apo >= matype):
-            print("comprar!")
+            print("buy condition 1!")
         else:
-            print("vender!")
-            
+            print("sell condition 1!")
+
+  #Variación del my_apo - apo_Hace15 >= APO_delta
+    if len(closes)-(15*60/2) > APO_slow: #Cambia la condición porque hay que tener las últimas 21 velas de 15 min, ya que no usamos la última de 15 min
+        closesHace15=closes.remove("Ultimo Dato")#No se como escribir que hay que sacar la última vela
+        apo_Hace15=talib.APO(closesHace15, APO_fast, APO_slow, matype)
+        if(my_apo-apo_Hace15>APO_delta):
+             print("buy condition 2!")
 
 def on_error(ws,error):
     print(error)
