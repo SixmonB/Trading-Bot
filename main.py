76d9f173396a8980418995d1bcfcd98f6393cee1
SixmonB <71516407+SixmonB@users.noreply.github.com>
2021-08-21 15:42:58 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,38 @@
+# info at https://github.com/binance-exchange/binance-official-api-docs
+# https://mrjbq7.github.io/ta-lib/
+# https://github.com/binance/binance-spot-api-docs/blob/master/web-socket-streams.md#klinecandlestick-streams
+# EMA (overlap), Absolute Price Oscillator (momentum), Bollinger bands (overlap)
+
+import config, websocket, json, numpy, talib, pprint
+
+SOCKET = "wss://stream.binance.com:9443/ws/adausdt@kline_1m"    # <symbol>@kline<interval>
+closes = []
+def on_open(ws):
+    print('opened conneciton')
+
+def on_close(ws):
+    print('closed connection')
+
+def on_message(ws, message):
+    global closes
+    print('received message')
+    json_message = json.loads(message)
+    pprint.pprint(json_message)
+
+    candle = json_message['k']
+    is_candle_closed = candle['x']
+    close = candle['c']
+
+    if is_candle_closed:
+        print("candle closed at {}".format(close))
+        closes.append(float(close))
+
+
+def on_error(ws,error):
+    print(error)
+
+websocket.enableTrace(True)
+ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
+ws.run_forever()
+
+
