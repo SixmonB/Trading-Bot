866c701034c1ab47ae426d1062e304e4072738ce
SixmonB <71516407+SixmonB@users.noreply.github.com>
2021-08-24 02:39:23 -0300

    Co-authored-by: Tomasmirchak <Tomasmirchak@users.noreply.github.com>

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-apiKey = "asd" #"0YE1PQZAftpbVT8aYmtkWxe6z1XP9snnWo1k3CqIIwLcxEQdosFZ2TXLSGSInu5R"
+apiKey = "0YE1PQZAftpbVT8aYmtkWxe6z1XP9snnWo1k3CqIIwLcxEQdosFZ2TXLSGSInu5R"
 apiSecurity = "BthbAtv8Eg2UC0emKbeupaJHYvZ3uITEl5qs4ysc5i22cIrpwME6GKfGDjccO60b"
--- testing.py
+++ testing.py
@@ -1,23 +1,6 @@
-# ------------- INFORMACION ------------- 
 
-# info at https://github.com/binance-exchange/binance-official-api-docs
-# https://mrjbq7.github.io/ta-lib/
-# https://github.com/binance/binance-spot-api-docs/blob/master/web-socket-streams.md#klinecandlestick-streams
-
-# --------------------------------------- 
-
-# ------------- FUNCIONES -------------
-
-# EMA 15
-# real = EMA(close, timeperiod=30)
-# apo 
-# real = APO(close, fastperiod=10, slowperiod=20, matype=0)
-# bbands 
-# upperband, middleband, lowerband = BBANDS(close, timeperiod=5, nbdevup=2, nbdevdn=2, matype=0)
-
-# --------------------------------------
-
-import config, websocket, json, numpy, talib, pprint
+import config, websockets, json, talib, pprint
+import numpy as np
 from binance.client import Client
 from binance.enums import *
 
@@ -27,7 +10,7 @@ closesHace15 = []
 # ------------- PARAMETROS -------------
 
 tiempo_vela = 15 #Duracion de la vela pensada para la estrategia en min 
-ajuste = tiempo_vela*60/2 #En el denominador indicar la duracion de la vela para operar en seg 
+ajuste = tiempo_vela*60/60 #En el denominador indicar la duracion de la vela para operar en seg 
 EMA_period = 18*ajuste
 APO_slow = 20*ajuste 
 APO_fast = 10*ajuste
@@ -45,41 +28,38 @@ compra = 0 # valor de inicializacion de la variable de compra
 client = Client(config.apiKey, config.apiSecurity, tld='com')
 symbolTicker = 'ADAUSDT'
 quantityOrders = 1
-candles_historical = client.get_historical_klines(symbolTicker, Client.KLINE_INTERVAL_1MINUTE, "5 hour ago UTC")
+candles_historical = client.get_historical_klines(symbolTicker, Client.KLINE_INTERVAL_1MINUTE, "15 hour ago UTC")
 
-def on_open(ws):
-    print('opened conneciton')
-
-def on_close(ws):
-    print('closed connection')
 
 def Average(lst):
     return sum(lst)/len(lst)
 
-def on_message(ws, message):
-    global closes # lista de cierres de vela
-    global compra
-    # print('received message')
-    json_message = json.loads(message)
-    pprint.pprint(json_message)
 
-    candle = json_message['k']
-    # is_candle_closed = candle['x']
-    closes.append(candle['c'])
-    
+
+closes = list()
+# print('received message')
+for i in range(len(candles_historical)):
+    closes.append(float(candles_historical[i][4]))
+    # print(closes)
     if len(closes) >= EMA_period:
-        my_ema = talib.EMA(closes, EMA_period)
-
+        float_closes = [float(x) for x in closes]
+        np_float_closes = np.array(float_closes)
+        my_ema = talib.EMA(np_float_closes, EMA_period)
+        print(".")
     if len(closes) >= APO_slow + ajuste:
-        closes_actual = closes[ajuste-1:len(closes)]
-        my_apo = talib.APO(closes_actual, APO_fast, APO_slow, matype)
+        closes_actual = closes[int(ajuste-1):int(len(closes))]
+        float_closes_actual = [float(x) for x in closes_actual]
+        np_float_closes_actual = np.array(float_closes_actual)
+        my_apo = talib.APO(np_float_closes_actual, APO_fast, APO_slow, matype)
             # Condicion de venta 1: APO con valor negativo
         if(my_apo >= matype):
             print("buy condition 1!")
             # Variación del my_apo - apo_Hace15 >= APO_delta
             # Cambia la condición porque hay que tener las últimas 21 velas de 15 min, ya que no usamos la última de 15 min
-            closes_Hace15 = closes[:len(closes)-ajuste]
-            apo_Hace15=talib.APO(closes_Hace15, APO_fast, APO_slow, matype)
+            closes_hace15 = closes[:len(closes)-ajuste]
+            float_closes_hace15 = [float(x) for x in closes_hace15]
+            np_float_closes_hace15 = np.array(float_closes_hace15)
+            apo_Hace15=talib.APO(np_float_closes_hace15, APO_fast, APO_slow, matype)
             if(my_apo-apo_Hace15 > APO_delta):
                 print("buy condition 2!")
         # Precio de vela creciente (buy condition 3)
@@ -96,6 +76,7 @@ def on_message(ws, message):
         # Opcion 1: Precio vela (-5)< EMA < Precio vela (0)
                     if (closes[-6] < my_ema and my_ema < closes[-1] ):
                         print("buy condition 4!")
+                        print("COMPRASTE")
                         compra = closes[-1]
         else:
             print("sell condition 1!")
@@ -120,14 +101,7 @@ def on_message(ws, message):
   # Opcion 2: Precio vela (0)*0.998 < BBands < Precio vela (0)*1.002
 
   #Evaluar la posibilidad de venta con BBands inferior (sell condition 4 - creo q no es necesaria- )
-      
-def on_error(ws,error):
-    print(error)
 
-websocket.enableTrace(True)
-ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
-ws.run_forever()
-
-
-
-
+#websocket.enableTrace(True)
+#ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
+#ws.run_forever()
