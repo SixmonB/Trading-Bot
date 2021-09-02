426d57d4817d412f15eb19dfb6c305d0c142eaa9
SixmonB <71516407+SixmonB@users.noreply.github.com>
2021-09-02 17:37:44 -0300

    cambios realizados en main y correccion de detalles

--- testing.py
+++ testing.py
@@ -39,7 +39,7 @@ minutes_venta3 = list()
 
 # cliente con informacion de cada cuenta de binance
 client = Client(config.apiKey, config.apiSecurity, tld='com')
-symbolTicker = 'MATICUSDT' # token a tradear
+symbolTicker = 'ADAUSDT' # token a tradear
 # velas historicas de la moneda indicada obtenidas de la api
 candles_historical = client.get_historical_klines(symbolTicker, Client.KLINE_INTERVAL_1MINUTE, "15 hour ago UTC")
 
@@ -68,97 +68,81 @@ for i in range(len(candles_historical)):
         my_ema_to_print = talib.EMA(np_float_closes_to_print, EMA_period)
         #print(my_ema)
         #print("closes: {}", len(np_float_closes))
-        if not (math.isnan(my_ema[-1])): # verificamos que el valor de my_ema/my_apo sea un numero real
-            #print(my_ema[-1])
-            #print(".")
-            if len(closes) >= APO_slow + ajuste:
-                closes_actual = closes[int(ajuste-1):int(len(closes))]
-                float_closes_actual = [float(x) for x in closes_actual]
-                np_float_closes_actual = np.array(float_closes_actual)
-                my_apo = talib.APO(np_float_closes_actual, APO_fast, APO_slow, matype)
-                my_apo_to_print = talib.APO(np_float_closes_to_print, APO_fast, APO_slow, matype)
-                if not (math.isnan(my_apo[-1])):
-                    #print(my_apo[-1])
-                    #print(".....")
-                    # Condicion de compra 1: APO con valor positivo
-                    if(my_apo[-1] >= matype):
-                        # print("buy condition 1!")
-                        # Variación del my_apo - apo_Hace15 >= APO_delta
-                        # Cambia la condición porque hay que tener las últimas 21 velas de 15 min, ya que no usamos la última de 15 min
-                        closes_hace15 = closes[:int(len(closes)-ajuste)]
-                        float_closes_hace15 = [float(x) for x in closes_hace15]
-                        np_float_closes_hace15 = np.array(float_closes_hace15)
-                        apo_hace15 = talib.APO(np_float_closes_hace15, APO_fast, APO_slow, matype)
-                        if not (math.isnan(apo_hace15[-1])):
-                            
-                            if(my_apo[-1]-apo_hace15[-1] > APO_delta):
-                                
-                                # print("buy condition 2!")
-                        # Precio de vela creciente (buy condition 3)
-                        # Promedio velas(-600;-400) < Promedio velas(-400;-200) < Promedio velas(-200;0)
-                                #velas_150_100 = closes[int(-ajuste*1.5-1):int(-ajuste-1)]
-                                #velas_100_50 = closes[int(-ajuste-1):int(-ajuste*0.5-1)]
-                                #velas_50_0 = closes[int(-ajuste*0.5-1):-1]
-                                #average_150 = Average(velas_150_100)
-                                #average_100 = Average(velas_100_50)
-                                #average_50 = Average(velas_50_0)
-                                #if (average_150 < average_100 and average_100 < average_50):
-                                    # print("buy condition 3!")
-                        # Precio de vela cruza con indicador EMA (buy condition 4)
-                        # Opcion 1: Precio vela (-5)< EMA < Precio vela (0)
-                                if (closes[-2] < my_ema[-1] and my_ema[-1] < closes[-1]):
-                                    if (money>0):
-                                        # print("buy condition 4!")
-                                        print("COMPRASTE")
-                                        compra = closes[-1]
-                                        compras.append(compra)
-                                        minutes_compra.append(minute)
-                                        ada = money/closes[-1]
-                                        money = 0
-                                        print("Compraste en el minuto: {}", minute)
-                                        print("ADA: {}", ada)
-                                        print("USDT: {}", money)
-                    else:
-                        if (ada>0):
-                            print("sell condition 1!")
-                            print("VENDISTE")
-                            venta = closes[-1]
-                            ventas1.append(venta)
-                            minutes_venta1.append(minute)
-                            money = ada*closes[-1]
-                            ada = 0
+    # if not (math.isnan(my_ema[-1])): # verificamos que el valor de my_ema/my_apo sea un numero real
+        if len(closes) >= APO_slow + ajuste:
+            closes_actual = closes[int(ajuste-1):int(len(closes))]
+            float_closes_actual = [float(x) for x in closes_actual]
+            np_float_closes_actual = np.array(float_closes_actual)
+            my_apo = talib.APO(np_float_closes_actual, APO_fast, APO_slow, matype)
+            my_apo_to_print = talib.APO(np_float_closes_to_print, APO_fast, APO_slow, matype)
+            # if not (math.isnan(my_apo[-1])):
+                # Condicion de compra 1: APO con valor positivo
+            if(my_apo[-1] >= matype):
+                # print("buy condition 1!")
+                # Variación del my_apo - apo_Hace15 >= APO_delta
+                # Cambia la condición porque hay que tener las últimas 21 velas de 15 min, ya que no usamos la última de 15 min
+                closes_hace15 = closes[:int(len(closes)-ajuste)]
+                float_closes_hace15 = [float(x) for x in closes_hace15]
+                np_float_closes_hace15 = np.array(float_closes_hace15)
+                apo_hace15 = talib.APO(np_float_closes_hace15, APO_fast, APO_slow, matype)
+                # if not (math.isnan(apo_hace15[-1])):
+                if(my_apo[-1]-apo_hace15[-1] > APO_delta):        
+            # print("buy condition 2!")
+            # Precio de vela cruza con indicador EMA (buy condition 4)
+            # Opcion 1: Precio vela (-5)< EMA < Precio vela (0)
+                    if (closes[-2] < my_ema[-1] and my_ema[-1] < closes[-1]):
+                        if (money>0):
+                            # print("buy condition 4!")
+                            print("COMPRASTE")
+                            compra = closes[-1]
+                            compras.append(compra)
+                            minutes_compra.append(minute)
+                            ada = money/closes[-1]
+                            money = 0
+                            print("Compraste en el minuto: {}", minute)
                             print("ADA: {}", ada)
                             print("USDT: {}", money)
-                        # Precio de vela cruza con indicador BBands (sell condition 1 - take profit - )
-                        # Opcion 1: Precio vela (-5)< BBands < Precio vela (0)
-                    upperband, middleband, lowerband = talib.BBANDS(np_float_closes, bbands_period, nbdevup, nbdevdn, matype)
-                    upperband_to_print, middleband_to_print, lowerband_to_print = talib.BBANDS(np_float_closes_to_print, bbands_period, nbdevup, nbdevdn, matype)
-                if not (math.isnan(upperband[-1])):    
-                    if (float_closes[-2] < upperband[-1] and upperband[-1] < float_closes[-1]):
-                        if (ada>0):
-                            print("sell condition 2!")
-                            print("VENDISTE")
-                            venta = closes[-1]
-                            ventas2.append(venta)
-                            minutes_venta2.append(minute)
-                            money = ada*closes[-1]
-                            ada = 0
-                            print("ADA: {}", ada)
-                            print("USDT: {}", money)
-                    # Stop loss -5% (sell condition 3)
-                    # Precio de vela (0) < Precio de vela de compra * 0.95
-                if (float_closes[-1] < compra*0.95):
-                    if (ada>0):
-                        print("sell condition 3!")
-                        print("VENDISTE")
-                        venta = closes[-1]
-                        ventas3.append(venta)
-                        minutes_venta3.append(minute)
-                        money = ada*closes[-1]
-                        ada = 0
-                        print("ADA: {}", ada)
-                        print("USDT: {}", money)
-                closes.pop(0)
+            else:
+                if (ada>0):
+                    print("sell condition 1!")
+                    print("VENDISTE")
+                    venta = closes[-1]
+                    ventas1.append(venta)
+                    minutes_venta1.append(minute)
+                    money = ada*closes[-1]
+                    ada = 0
+                    print("ADA: {}", ada)
+                    print("USDT: {}", money)
+                # Precio de vela cruza con indicador BBands (sell condition 1 - take profit - )
+                # Opcion 1: Precio vela (-5)< BBands < Precio vela (0)
+            upperband, middleband, lowerband = talib.BBANDS(np_float_closes, bbands_period, nbdevup, nbdevdn, matype)
+            upperband_to_print, middleband_to_print, lowerband_to_print = talib.BBANDS(np_float_closes_to_print, bbands_period, nbdevup, nbdevdn, matype)
+            #if not (math.isnan(upperband[-1])):    
+            if (float_closes[-2] < upperband[-1] and upperband[-1] < float_closes[-1]):
+                if (ada>0):
+                    print("sell condition 2!")
+                    print("VENDISTE")
+                    venta = closes[-1]
+                    ventas2.append(venta)
+                    minutes_venta2.append(minute)
+                    money = ada*closes[-1]
+                    ada = 0
+                    print("ADA: {}", ada)
+                    print("USDT: {}", money)
+                # Stop loss -5% (sell condition 3)
+                # Precio de vela (0) < Precio de vela de compra * 0.95
+            if (float_closes[-1] < compra*0.95):
+                if (ada>0):
+                    print("sell condition 3!")
+                    print("VENDISTE")
+                    venta = closes[-1]
+                    ventas3.append(venta)
+                    minutes_venta3.append(minute)
+                    money = ada*closes[-1]
+                    ada = 0
+                    print("ADA: {}", ada)
+                    print("USDT: {}", money)
+            closes.pop(0)
     minute = minute+1
 plt.figure(1)
 plt.plot(my_ema_to_print, label='EMA')
@@ -170,9 +154,12 @@ plt.plot(minutes_compra, compras, 'o', color = 'green')
 plt.plot(minutes_venta1, ventas1, 'x', color='red') # sell condition 1: apo negativo
 plt.plot(minutes_venta2, ventas2, '^', color='red') # sell condition 2: toca upperband
 plt.plot(minutes_venta3, ventas3, '*', color='red') # sell condition 3: stop loss
+plt.xlim([0, 900])
 plt.legend(loc='best')
 plt.figure(2)
 plt.plot(my_apo_to_print, label='APO')
+plt.axhline(y=0, color='red')
+plt.xlim([0, 900])
 plt.show()
   # Opcion 2: Precio vela (0)*0.998 < EMA < Precio vela (0)*1.002
 
